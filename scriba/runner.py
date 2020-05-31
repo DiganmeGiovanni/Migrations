14110a1475be4e017106d40693da9aec0355d127
Giovanni Aguirre <giovanni.fi05@gmail.com>
2020-05-31 17:01:08 -0500

    :rocket: Right usage of positional and optional arguments

--- scriba/runner.py
+++ scriba/runner.py
@@ -5,23 +5,25 @@ parser = argparse.ArgumentParser()
 file_path = "./migrations.yml"
 
 parser.add_argument(
-    "config",
+    "action",
+    type=str,
+    choices=['status', 'migrate', 'rollback'],
+    help="Action to execute"
+)
+
+parser.add_argument(
+    "--config",
+    "-c",
     type=str,
     help="Scriba configurations file. Default: ./migrations.yml"
 )
 
 parser.add_argument(
-    "action",
-    type=str,
-    help="Action to execute [status | migrate | rollback]"
+    "--steps",
+    "-s",
+    type=int,
+    help="Number of scripts to run"
 )
-
-# parser.add_argument(
-#     "-s",
-#     "--steps",
-#     type=int,
-#     help="Number of scripts to run"
-# )
 
 args = parser.parse_args()
 
@@ -34,7 +36,7 @@ if args.action == "status":
     scriba.status()
 
 if args.action == "migrate":
-    scriba.migrate()
+    scriba.migrate(steps=args.steps)
 
 if args.action == "rollback":
-    scriba.rollback()
+    scriba.rollback(steps=args.steps)
